ee23c72a953408bc3b3de0e1e499cd45d4689bb2
karnavat dilip <dilipkarnavat958@gmail.com>
2022-07-08 10:18:19 +0530

    the first commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 const Pool = pg.Pool
 const app = express()
-const port = process.env.PORT;
+const port = process.env.PORT || 9898;
 
 // var con = new Pool({
 //   host: "localhost",
@@ -24,30 +24,24 @@ const port = process.env.PORT;
 const devconfig=`postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
 const proconfig=process.env.DATABASE_URL;
 const con = new Pool({
-  connectionString:process.env.NODE_ENV==="production"?proconfig:devconfig,
-  // ssl:{
-  //   rejectUnauthorized:false
-  // }
-  // ssl: process.env.DATABASE_URL ? true : false
+  connectionString:process.env.NODE_ENV==="production"?proconfig:devconfig
+  // ssl:true
 })
 con.connect(function (err) {
   if (err) throw err;
   console.log("Connected successfully!");
 });
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static());
-  app.get("/",(req,res)=>{
-    res.sendFile(path.join(path.dirname(fileURLToPath(import.meta.url)),"Client/build/index.html"));
-  })
+    app.use(express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), "Client/build")));
 }
 console.log('!!!',path.join(path.dirname(fileURLToPath(import.meta.url)), "Client/build"));
 // console.log(__dirname);
-app.use(cors({origin:'http://localhost:1010/', credentials:true}));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use("/", router)
+app.use('/',router)
 
 app.listen(port, () => {
   console.log(`server is listening on port ${port}`)
